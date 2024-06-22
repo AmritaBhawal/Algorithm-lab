1e5a7dca9ff297e8219ce41f0f3b242762762c15
AmritaBhawal <bhawalamrita09@gmail.com>
2024-06-22 19:52:27 +0600

    Add files via upload

---
+++ Swap.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Before swapping : " << num1 << " " << num2 << endl;
+
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout << "After swapping :" << num1 << " " << num2 << endl;
+
+    return 0;
+}
+
